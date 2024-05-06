c8485557883a873d4527897d78fe489ff520ab68
Sahaj <sahajjain13@gmail.com>
2024-05-06 23:52:03 +0530

    feat: Add shadcnui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import init, { execute } from "altr-wasm";
+import "./index.css";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export const App: React.FC = () => {
+    const [candidate, setCandidate] = useState("");
+    const [rename, setRename] = useState("");
+    const [buf, setBuf] = useState("");
     const [ans, setAns] = useState("");
-    useEffect(() => {
-        init().then(() => {
-            setAns(
-                execute(
-                    "function",
-                    "myNewFunc",
-                    `
-fucntion this is the new thing
-and 
-wow function and FUNCTION
-			`
-                )
-            );
-        });
-    }, []);
 
-    return <div className="p-40">{ans}</div>;
+    const handleSubmit = () => {
+        init().then(() => {
+            setAns(execute(candidate, rename, buf));
+        });
+    };
+
+    return (
+        <div className="p-12">
+            <div className="mb-6">
+                <Input name="candidate" onChange={(ev) => setCandidate(ev.target.value)} />
+            </div>
+            <div className="mb-6">
+                <Input name="rename" onChange={(ev) => setRename(ev.target.value)} />
+            </div>
+            <div className="mb-6">
+                <Input name="buf" onChange={(ev) => setBuf(ev.target.value)} />
+            </div>
+
+            <Button onClick={handleSubmit}>Submit</Button>
+            <div>{ans}</div>
+        </div>
+    );
 };
