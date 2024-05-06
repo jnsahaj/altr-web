f769bbe140099cb07c82ba59931606e6234f49bc
Sahaj <sahajjain13@gmail.com>
2024-05-07 00:43:56 +0530

    feat: Experiment with dialog component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,20 @@ import init, { execute } from "altr-wasm";
 import "./index.css";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from "./components/ui/dialog";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { ArrowRight, Pencil } from "lucide-react";
 
 export const App: React.FC = () => {
-    const [candidate, setCandidate] = useState("");
-    const [rename, setRename] = useState("");
+    const [candidate, setCandidate] = useState("user");
+    const [rename, setRename] = useState("new_user_18");
     const [buf, setBuf] = useState("");
     const [ans, setAns] = useState("");
 
@@ -18,18 +28,54 @@ export const App: React.FC = () => {
 
     return (
         <div className="p-12">
-            <div className="mb-6">
-                <Input name="candidate" onChange={(ev) => setCandidate(ev.target.value)} />
-            </div>
-            <div className="mb-6">
-                <Input name="rename" onChange={(ev) => setRename(ev.target.value)} />
-            </div>
-            <div className="mb-6">
-                <Input name="buf" onChange={(ev) => setBuf(ev.target.value)} />
-            </div>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button variant="outline" className="mb-12 p-8">
+                        <div className="flex gap-4 items-center text-2xl">
+                            <div>{candidate}</div>
+                            <div>
+                                <ArrowRight />
+                            </div>
+                            <div>{rename}</div>
+                            <Pencil />
+                        </div>
+                    </Button>
+                </DialogTrigger>
+                <div className="mb-6">
+                    <Input name="buf" onChange={(ev) => setBuf(ev.target.value)} />
+                </div>
 
-            <Button onClick={handleSubmit}>Submit</Button>
-            <div>{ans}</div>
+                <Button onClick={handleSubmit}>Submit</Button>
+                <div>{ans}</div>
+
+                <DialogContent className="sm:max-w-[425px]">
+                    <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>
+                            Make changes to your profile here. Click save when you are done.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="flex gap-4 py-4">
+                        <div className="items-center gap-4">
+                            <Input
+                                name="candidate"
+                                value={candidate}
+                                onChange={(ev) => setCandidate(ev.target.value)}
+                            />
+                        </div>
+                        <div className="items-center gap-4">
+                            <Input
+                                name="rename"
+                                value={rename}
+                                onChange={(ev) => setRename(ev.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
         </div>
     );
 };
