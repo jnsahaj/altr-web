3c848ec9d3722d6b643228b0692382715ed488db
Sahaj <sahajjain13@gmail.com>
2024-05-09 15:02:44 +0530

    feat: Error handling and basic pass out basic records from wasm

--- altr-wasm/src/lib.rs
+++ altr-wasm/src/lib.rs
@@ -1,10 +1,69 @@
-use altr::task::Task;
+use altr::{record::Records, task::Task};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn execute(candidate: String, rename: String, buf: String) -> String {
-    let mut task = Task::build(&candidate, &rename, &buf).unwrap();
-    let mut records = task.generate_records().unwrap();
-    let processed_buf = task.process_records(&mut records);
-    processed_buf
+pub fn execute(candidate: String, rename: String, buf: String) -> Result<AltrResult, AltrError> {
+    let mut task = Task::build(&candidate, &rename, &buf).map_err(|e| match e {
+        altr::Error::CandidateCasing(_) => AltrError::CandidateCasing,
+        altr::Error::RenameCasing(_) => AltrError::RenameCasing,
+        _ => AltrError::Generic,
+    })?;
+    let mut records = task.generate_records();
+    let (processed_buf, processed_records) = task.process_records(&mut records);
+    Ok(AltrResult {
+        records: parse_records(records),
+        processed_buf,
+        processed_records: parse_records(processed_records),
+    })
+}
+
+#[wasm_bindgen]
+pub enum AltrError {
+    CandidateCasing,
+    RenameCasing,
+    Generic,
+}
+
+#[wasm_bindgen]
+pub struct AltrResult {
+    processed_buf: String,
+    records: Vec<BasicRecord>,
+    processed_records: Vec<BasicRecord>,
+}
+
+#[wasm_bindgen]
+impl AltrResult {
+    #[wasm_bindgen(getter = processed_buf)]
+    pub fn get_processed_buf(&self) -> String {
+        self.processed_buf.clone()
+    }
+
+    #[wasm_bindgen(getter = records)]
+    pub fn get_records(&self) -> Vec<BasicRecord> {
+        self.records.clone()
+    }
+
+    #[wasm_bindgen(getter = processed_records)]
+    pub fn get_processed_records(&self) -> Vec<BasicRecord> {
+        self.processed_records.clone()
+    }
+}
+
+#[wasm_bindgen]
+#[derive(Clone)]
+pub struct BasicRecord {
+    pub pos: usize,
+    pub len: usize,
+}
+
+fn parse_records(records: Records) -> Vec<BasicRecord> {
+    let mut result = Vec::new();
+    for (_, record) in records.iter() {
+        result.push(BasicRecord {
+            pos: record.pos,
+            len: record.len,
+        })
+    }
+
+    result
 }
